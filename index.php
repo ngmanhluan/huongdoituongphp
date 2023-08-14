8449057fc6cdb43b10545ca271ed7c57d9e92b12
manhluannguyen <ngmanhluan01@gmail.com>
2023-08-14 11:03:58 +0700

    done

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+class Loan {
+    public $id_loan;
+    public $date;
+    public $num;
+
+    public function __construct($id, $date, $num) {
+        $this->id_loan = $id;
+        $this->date = $date;
+        $this->num = $num;
+    }
+
+    public function displayLoan_BookInfo() {
+        echo "Loan ID: " . $this->id_loan . "<br>";
+        echo "Date: " . $this->date . "<br>";
+        echo "Number of Books: " . $this->num . "<br>";
+    }
+}
+?>
