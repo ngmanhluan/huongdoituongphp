a0d7cf81a5a51b4dc380aa0fed1115315da07b14
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2023-08-14 18:57:53 +0700

    Show Information

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    include 'Library_Book';
+    include 'Library_Book.php';
     $o = new Library_Book("LBR1",1,"01/01/2015",2,"BOOK1","POOR DAD RICH DAD","Kim Dong","02/01/2020",20000);
     $o->displayLib_BookInfo();
 ?>
