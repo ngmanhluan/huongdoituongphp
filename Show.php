2ea41e1edb29a2f5b7f34ee55cfa44755e8e4eb2
manhluannguyen <ngmanhluan01@gmail.com>
2023-08-14 16:41:01 +0700

    done

--- Show.php
+++ Show.php
@@ -3,7 +3,7 @@ include "./Author.php" ;
 include "./Loan.php";
 include "./student.php";
 
-$Author = new Author("Hà Phi Dũng", "nam", "1-9-2004", "TTAL");
+$Author = new Author(" Hà Phi Dũng", "nam", " 1-9-2004", " TTAL");
 echo $Author->Display_Author();
 echo "###------###" . "<br>";
 
