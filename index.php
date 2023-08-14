3c506c895e0604e2abf010d71b10b8d854c7ad70
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2023-08-14 18:57:28 +0700

    Show info

--- index.php
+++ index.php
@@ -1,52 +1,5 @@
 <?php 
-    class Book{
-        private $isbn;
-        private $title;
-        private $proceducer;
-        private $year;
-        private $price;
-        function __construct($isbn,$title,$proceducer,$year,$price){
-            $this->isbn = $isbn;
-            $this->title = $title;
-            $this->proceducer = $proceducer;
-            $this->year = $year;
-            $this->price = $price;
-        }
-        function displayBookInfo(){
-            echo "<strong>ISBN:</strong> ".$this->isbn."<br>";
-            echo "<strong>Title:</strong> ".$this->title."<br>";
-            echo "<strong>Proceducer:</strong> ".$this->proceducer."<br>";
-            echo "<strong>Year:</strong> ".$this->year."<br>";
-            echo "<strong>Price:</strong> ".$this->price."<br>";
-        }
-    }
-    class Library_Book extends Book
-    {
-        private $id_lib_book;
-        private $position;
-        private $date;
-        private $quanity;
-        function __construct($id_lib_book,$position,$date,$quanity,$isbn,$title,$proceducer,$year,$price){
-            parent::__construct($isbn,$title,$proceducer,$year,$price);
-            $this->id_lib_book = $id_lib_book;
-            $this->position = $position;
-            $this->date = $date;
-            $this->quanity = $quanity;
-        }
-        function getQuanity(){
-            return $this->quanity;
-        }
-        function setQuanity($quanity){
-            $this->quanity = $quanity;
-        }
-        function displayLib_BookInfo(){
-            parent::displayBookInfo();
-            echo "<strong>ID: </strong> $this->id_lib_book <br>";
-            echo "<strong>Position: </strong> $this->position <br>";
-            echo "<strong>Date: </strong> $this->date <br>";
-            echo "<strong>Quanity: </strong> $this->quanity <br>";
-        }
-    }
+    include 'Library_Book';
     $o = new Library_Book("LBR1",1,"01/01/2015",2,"BOOK1","POOR DAD RICH DAD","Kim Dong","02/01/2020",20000);
     $o->displayLib_BookInfo();
 ?>
