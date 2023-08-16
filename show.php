cba5b118fe6587d08e421549d172c6437ba514d6
Vu tran <levu1962004@gmail.com>
2023-08-16 09:23:10 +0700

    done

--- show.php
+++
@@ -1,8 +0,0 @@
-<?php
- include "./Category.php";
-
- $category  = new Category ();
- $category -> Category ("1","danhmuc", "jjdsdjsjd");
- echo $category -> Display_Category_Info();
- echo "### .......####" . "<br>";
-?>
